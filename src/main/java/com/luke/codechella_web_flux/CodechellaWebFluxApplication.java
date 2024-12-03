581af4597e6cc5fed8749f6421bb3af8a8c72e62
Victor Yunes Ribeiro Von Melentovytch <victor.melentovytch@tjrj.jus.br>
2024-12-03 17:12:26 -0300

    first commit

---
+++ src/main/java/com/luke/codechella_web_flux/CodechellaWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.luke.codechella_web_flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodechellaWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodechellaWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luke/codechella_web_flux/CodechellaWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luke.codechella_web_flux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodechellaWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
